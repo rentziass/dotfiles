b4303b1caa02242d042bdee9d75a2759e0f5243f
Francesco Renzi <rentziass@gmail.com>
2022-06-15 17:04:27 +0100

    exit vim when packer is installed

--- vim/.config/nvim/lua/rentziass/plugins/bundle.lua
+++ vim/.config/nvim/lua/rentziass/plugins/bundle.lua
@@ -4,7 +4,8 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+  vim.cmd [[qa]]
 end
 
 return require('packer').startup(function(use)
