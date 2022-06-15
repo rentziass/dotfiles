2a923702f307d442b9a9b0d2d8f11476fac12793
Francesco Renzi <rentziass@gmail.com>
2022-06-15 17:36:56 +0100

    empty line

--- vim/.config/nvim/lua/rentziass/plugins/bundle.lua
+++ vim/.config/nvim/lua/rentziass/plugins/bundle.lua
@@ -1,6 +1,7 @@
 -- To install packer:
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 --  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
