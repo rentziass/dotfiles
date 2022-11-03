54d862521ee397848497b4d2b436f9c3c4a08614
Francesco Renzi <rentziass@gmail.com>
2022-11-03 08:47:56 +0000

    fix deprecation warning

--- vim/.config/nvim/lua/rentziass/plugins/cmp.lua
+++ vim/.config/nvim/lua/rentziass/plugins/cmp.lua
@@ -122,7 +122,7 @@ cmp.setup.cmdline(':', {
   })
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local servers = { 'gopls', 'sumneko_lua', 'tsserver', 'yamlls', 'rust_analyzer' }
 for _, server in pairs(servers) do
   require('lspconfig')[server].setup {
