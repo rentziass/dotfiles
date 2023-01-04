7f4b62fe2445f2952613d5a7f71568c167a79795
Francesco Renzi <rentziass@gmail.com>
2023-01-04 15:36:53 +0100

    More telescope

--- vim/.config/nvim/lua/rentziass/plugins/telescope.lua
+++ vim/.config/nvim/lua/rentziass/plugins/telescope.lua
@@ -1,10 +1,14 @@
 local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+local themes = require("telescope.themes")
 
 vim.keymap.set("n", "<leader>f", builtin.find_files)
 vim.keymap.set("n", "<leader>g", builtin.live_grep)
 vim.keymap.set("n", "<leader>F", builtin.resume)
 vim.keymap.set("n", "<leader>B", builtin.buffers)
+vim.keymap.set("n", '<leader>"', function()
+  builtin.registers(themes.get_cursor())
+end)
 
 telescope.setup({
   pickers = {
@@ -28,6 +32,11 @@ telescope.setup({
                 }
             end
         },
+    },
+    extensions = {
+      ["ui-select"] = {
+        themes.get_cursor({}),
+      },
     },
 })
 
