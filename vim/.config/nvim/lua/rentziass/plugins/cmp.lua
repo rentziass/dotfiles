70a8b0252a3cda220345bb52c3d387e321f878ce
Francesco Renzi <rentziass@gmail.com>
2023-02-01 10:51:14 +0100

    stop auto completion submission on cmdline

--- vim/.config/nvim/lua/rentziass/plugins/cmp.lua
+++ vim/.config/nvim/lua/rentziass/plugins/cmp.lua
@@ -9,7 +9,7 @@ cmp.setup {
     ["<CR>"] = cmp.mapping(
       cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
+        select = false,
       },
       { "i", "c" }
     ),
