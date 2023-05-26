14b40a7999696af45a0584718e8e1bd97394b80e
Francesco Renzi <rentziass@gmail.com>
2023-05-26 11:29:50 +0100

    get out of the way, format

--- vim/.config/nvim/lua/rentziass/vscode/init.lua
+++ vim/.config/nvim/lua/rentziass/vscode/init.lua
@@ -17,7 +17,7 @@ keymap('n', '<Leader>s', ':w<CR>', { silent = true }) -- save
 keymap('n', '<Leader>xr', notify 'references-view.findReferences', { silent = true }) -- language references
 keymap('n', 'gr', notify 'editor.action.goToReferences', { silent = true })
 keymap('n', '<Leader>rn', notify 'editor.action.rename', { silent = true })
-keymap('n', '<Leader>fm', notify 'editor.action.formatDocument', { silent = true })
+keymap('n', '<Leader>Fm', notify 'editor.action.formatDocument', { silent = true })
 keymap('n', '<Leader>ca', notify 'editor.action.refactor', { silent = true }) -- language code actions
 keymap('n', ']d', notify 'editor.action.marker.next', { silent = true }) -- next diagnostic
 keymap('n', '[d', notify 'editor.action.marker.prev', { silent = true }) -- previous diagnostic
@@ -35,4 +35,4 @@ keymap('n', '<C-n>', notify 'workbench.action.toggleSidebarVisibility', { silent
 keymap('n', '<Leader>nf', notify 'workbench.files.action.showActiveFileInExplorer', { silent = true })
 
 keymap('n', '<Leader>th', notify 'workbench.action.toggleAuxiliaryBar', { silent = true }) -- toggle docview (help page)
-keymap('n', '<C-w>o', notify 'workbench.action.closeOtherEditors .', { silent = true }) -- close everything but current "split"
+keymap('n', '<C-w>o', notify 'workbench.action.closeOtherEditors', { silent = true }) -- close everything but current "split"
