0f9078f1641e1b78fd4ca796609850c67f1ffe0d
Francesco Renzi <rentziass@gmail.com>
2023-05-26 11:15:35 +0100

    make vscode feel a bit more like home

--- vim/.config/nvim/lua/rentziass/vscode/init.lua
+++ vim/.config/nvim/lua/rentziass/vscode/init.lua
@@ -12,18 +12,26 @@ end
 
 keymap('n', '<Leader>s', ':w<CR>', { silent = true }) -- save
 
+-- LSP stuff
+-- gd (go to definition) works by default
 keymap('n', '<Leader>xr', notify 'references-view.findReferences', { silent = true }) -- language references
 keymap('n', 'gr', notify 'editor.action.goToReferences', { silent = true })
 keymap('n', '<Leader>rn', notify 'editor.action.rename', { silent = true })
 keymap('n', '<Leader>fm', notify 'editor.action.formatDocument', { silent = true })
 keymap('n', '<Leader>ca', notify 'editor.action.refactor', { silent = true }) -- language code actions
-
-keymap('n', '<Leader>fg', notify 'workbench.action.findInFiles', { silent = true }) -- use ripgrep to search files
-keymap('n', '<Leader>ts', notify 'workbench.action.toggleSidebarVisibility', { silent = true })
-keymap('n', '<Leader>sn', notify 'workbench.files.action.showActiveFileInExplorer', { silent = true })
-keymap('n', '<Leader>th', notify 'workbench.action.toggleAuxiliaryBar', { silent = true }) -- toggle docview (help page)
-keymap('n', '<Leader>fc', notify 'workbench.action.showCommands', { silent = true }) -- find commands
-keymap('n', '<Leader>ff', notify 'workbench.action.quickOpen', { silent = true }) -- find files
-
+keymap('n', ']d', notify 'editor.action.marker.next', { silent = true }) -- next diagnostic
+keymap('n', '[d', notify 'editor.action.marker.prev', { silent = true }) -- previous diagnostic
 keymap('v', '<Leader>ca', v_notify 'editor.action.refactor', { silent = true })
-keymap('v', '<Leader>fc', v_notify 'workbench.action.showCommands', { silent = true })
+
+-- Telescope things
+keymap('n', '<Leader>f', notify 'workbench.action.quickOpen', { silent = true }) -- find files
+keymap('n', '<Leader>g', notify 'workbench.action.findInFiles', { silent = true }) -- search in files
+keymap('n', '<Leader>o', notify 'workbench.action.gotoSymbol', { silent = true }) -- search symbols in file
+keymap('v', '<Leader>p', v_notify 'workbench.action.showCommands', { silent = true })
+
+-- VSCode view management
+-- Nvim tree equivalents
+keymap('n', '<C-n>', notify 'workbench.action.toggleSidebarVisibility', { silent = true })
+keymap('n', '<Leader>nf', notify 'workbench.files.action.showActiveFileInExplorer', { silent = true })
+
+keymap('n', '<Leader>th', notify 'workbench.action.toggleAuxiliaryBar', { silent = true }) -- toggle docview (help page)
