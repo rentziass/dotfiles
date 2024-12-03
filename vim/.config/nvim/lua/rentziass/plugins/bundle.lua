f3c64845ba10b1273f516df35d95a1142c76247c
Francesco Renzi <rentziass@gmail.com>
2024-12-03 09:24:57 +0000

    bundle

--- vim/.config/nvim/lua/rentziass/plugins/bundle.lua
+++ vim/.config/nvim/lua/rentziass/plugins/bundle.lua
@@ -481,7 +481,20 @@ telescope.load_extension("ui-select")
     'towolf/vim-helm',
     ft = 'yaml',
   },
+
   'github/copilot.vim',
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 
   {
     'ThePrimeagen/harpoon',
