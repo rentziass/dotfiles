59f86f7fe32cc8895ff1662bc2fe73c4f36a7cac
Francesco Renzi <rentziass@gmail.com>
2026-01-14 19:33:19 +0000

    update and fix treesitter

--- vim/.config/nvim/lua/rentziass/plugins/treesitter.lua
+++ vim/.config/nvim/lua/rentziass/plugins/treesitter.lua
@@ -1,35 +1,26 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter', -- just the best thing
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     build = ':TSUpdate',
-    event = "BufReadPre",
+    lazy = false,
     config = function()
-      require 'nvim-treesitter.config'.setup {
-        ensure_installed = {
-          "go",
-          "c_sharp",
-          "rust",
-          "typescript",
-          "bash",
-          "dockerfile",
-          "graphql",
-          "hcl", -- Hashicorp language (used by Terraform)
-          "markdown",
-          "devicetree",
-          "ruby",
-          "vimdoc",
-          "zig",
-          "dart",
-        },
-        highlight = {
-          enable = true, -- false will disable the whole extension
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-      }
+      require('nvim-treesitter').install({
+        "go",
+        "c_sharp",
+        "rust",
+        "typescript",
+        "bash",
+        "dockerfile",
+        "graphql",
+        "hcl", -- Hashicorp language (used by Terraform)
+        "markdown",
+        "devicetree",
+        "ruby",
+        "vimdoc",
+        "zig",
+        "dart",
+      })
     end,
   },
   {
