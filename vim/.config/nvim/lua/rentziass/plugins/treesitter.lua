e1b92d582215c80638df9cc19018cf8d818b278c
Francesco Renzi <rentziass@gmail.com>
2026-02-24 10:03:23 +0000

    Fix treesitter

--- vim/.config/nvim/lua/rentziass/plugins/treesitter.lua
+++ vim/.config/nvim/lua/rentziass/plugins/treesitter.lua
@@ -21,6 +21,12 @@ return {
         "zig",
         "dart",
       })
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
     end,
   },
   {
